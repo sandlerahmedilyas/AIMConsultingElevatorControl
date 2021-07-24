caefd0eac7f6fd9ac8ab3568c387a27584140ddb
Ahmed Ilyas <ahmed@sandlersoftware.net>
2021-07-24 00:44:40 -0700

    added logger abstraction and injecting elevator service in web API controller.

--- Elevator.Implementation/ElevatorService.cs
+++ Elevator.Implementation/ElevatorService.cs
@@ -1,11 +1,19 @@
 ﻿using ElevatorControl.Contracts.Interfaces;
 using ElevatorControl.Contracts.RnR;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace ElevatorControl.Implementation
 {
     public class ElevatorService : IElevatorService
     {
+        private readonly ILogger<ElevatorService> _logger;
+
+        public ElevatorService(ILogger<ElevatorService> logger)
+        {
+            this._logger = logger;
+        }
+
         public RequestElevatorToFloorResponse RequestElevatorToFloor(RequestElevatorToFloorRequest request)
         {
             throw new NotImplementedException();
--- ElevatorControlService/Controllers/ElevatorController.cs
+++ ElevatorControlService/Controllers/ElevatorController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ElevatorControl.Contracts.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,10 +13,18 @@ namespace ElevatorControlService.Controllers
     public class ElevatorController : ControllerBase
     {
         private readonly ILogger<ElevatorController> _logger;
+        private readonly IElevatorService _elevatorService = null;
 
-        public ElevatorController(ILogger<ElevatorController> logger)
+        public ElevatorController(ILogger<ElevatorController> logger, IElevatorService elevatorService)
         {
             _logger = logger;
+
+            if (this._elevatorService == null)
+            {
+                throw new ArgumentNullException("Eleavor service must be supplied!", nameof(elevatorService));
+            }
+
+            this._elevatorService = elevatorService;
         }
     }
 }
